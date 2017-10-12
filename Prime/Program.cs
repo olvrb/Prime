8601c7570e831fdcad361a4381d504aa7c2c791c
olvrb <oboudet1@me.com>
2017-10-12 17:25:35 +0200

    made it a bit shorter

--- Prime/Program.cs
+++ Prime/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
+using System.Numerics;
 
 namespace Prime
 {
@@ -12,13 +13,20 @@ namespace Prime
     {
         static void Main(string[] args)
         {
-            File.Delete(@"C:\Users\oliver.boudet\PrimeNumbers.txt");
+            File.Delete("PrimeNumbers.txt");
+            init:
             Console.WriteLine("Where do you want to write the prime numbers and factors? (txt or console)");
             string whereToWrite = Console.ReadLine();
-
-            System.Numerics.BigInteger number = 0;
-            System.Numerics.BigInteger i = 2;
-            System.Numerics.BigInteger primeNumberNum = 0;
+            if (whereToWrite != "txt" && whereToWrite != "console")
+            {
+                Console.WriteLine("txt or console?");
+                Thread.Sleep(5000);
+                Console.Clear();
+                goto init;
+            }
+            BigInteger number = 0;
+            BigInteger i = 2;
+            BigInteger primeNumberNum = 0;
             while (true)
             {
                 i++;
@@ -32,11 +40,11 @@ namespace Prime
                     }
                 }
                 if (isPrime)
-                {                                                                                          //TODO fix delete error with NonPrimeNumbers.txt (line 18)
+                {                                                                                          //~~ TODO fix delete error with NonPrimeNumbers.txt (line 18)~~ 
                     if (whereToWrite == "txt")
                     {
                         using (System.IO.StreamWriter file =
-                        new System.IO.StreamWriter(@"C:\Users\oliver.boudet\PrimeNumbers.txt", true))
+                            new System.IO.StreamWriter("PrimeNumbers.txt", true))
                         {
                             file.WriteLine("Prime Number " + primeNumberNum + ": " + i);
                         }
@@ -44,6 +52,7 @@ namespace Prime
                     else if (whereToWrite == "console")
                     {
                         Console.WriteLine("Prime Number " + primeNumberNum + ": " + i);
+                        Console.WriteLine(" ");
                     }
                     primeNumberNum++;
                 }
@@ -52,7 +61,7 @@ namespace Prime
                     if (whereToWrite == "txt")
                     {
                         NonPrimeFile.WriteLine(i + " with factors: ");
-                        NonPrimeFile.WriteLine(ifPrimeFactors(i, whereToWrite));             //write to console, 1.0 test
+                        NonPrimeFile.WriteLine(ifPrimeFactors(i, whereToWrite));             //write to txt, 2.0 final
                         NonPrimeFile.WriteLine(" ");
                     }
                     else if (whereToWrite == "console")
@@ -62,12 +71,13 @@ namespace Prime
                         Console.WriteLine(" ");
                     }
                 }
+
             }
         }
         
-        public static System.IO.StreamWriter NonPrimeFile = new System.IO.StreamWriter(@"C:\Users\oliver.boudet\NonPrimeNumbers.txt", false);
+        public static System.IO.StreamWriter NonPrimeFile = new System.IO.StreamWriter("NonPrimeNumbers.txt", false);
 
-        public static string ifPrimeFactors(System.Numerics.BigInteger number, string whereToWrite)
+        public static string ifPrimeFactors(BigInteger number, string whereToWrite)
         {
             for (int i = 2; i < (number); i++)
             {
@@ -90,7 +100,7 @@ namespace Prime
             return number.ToString();
         }
 
-        public static bool IsPrime(System.Numerics.BigInteger number)
+        public static bool IsPrime(BigInteger number)
         {
             if (number % 2 == 0 && number != 2) { return false; }
 
